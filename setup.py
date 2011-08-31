874068529ced63a54dde1ffddeacc41514e37de8
gfleischer <gfleischer@1650b48b-b3f2-e7c8-9b9e-02247df4ad69>
2011-08-31 02:57:55 +0000

    Added support for py2app

--- setup.py
+++ setup.py
@@ -1,3 +1,25 @@
+#!/usr/bin/env python
+#
+# Author: Gregory Fleischer (gfleischer@gmail.com)
+#
+# Copyright (c) 2011 RAFT Team
+#
+# This file is part of RAFT.
+#
+# RAFT is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# RAFT is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RAFT.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 from distutils.core import setup
 import sys
 import os
@@ -41,13 +63,30 @@ def get_datafiles():
             data_files.append((key, datalist[key]))
     return data_files
 
+data_files = get_datafiles()
+
 if 'darwin' == sys.platform:
-    pass
+    import py2app
+    setup(
+        name = 'RAFT',
+        app = ['raft.pyw'],
+        data_files = data_files,
+          options = {
+            'py2app': 
+            {
+                'includes': ['lxml.etree', 'lxml._elementpath', 'gzip', 'sip'],
+                }
+            }
+        )
+    # need to customize qt.conf to avoid dup library loading
+    fh = open('dist/RAFT.app/Contents/Resources/qt.conf', 'wb')
+    fh.write('')
+    fh.close()
+
 elif 'win32' == sys.platform:
     import py2exe
 
     find_MSVCP90()
-    data_files = get_datafiles()
 
     setup(
         name = 'RAFT',
