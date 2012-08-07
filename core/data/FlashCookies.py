b4b832d0623ba329da42eb193100983f86e1ba69
gfleischer <gfleischer@1650b48b-b3f2-e7c8-9b9e-02247df4ad69>
2012-08-07 01:37:35 +0000

    Search for Flash Local Shared Objects in more locations

--- core/data/FlashCookies.py
+++ core/data/FlashCookies.py
@@ -56,33 +56,46 @@ class FlashCookies:
                 if entry not in ('.','..'):
                     obj['randomized'] = entry
             if entry.endswith('.sol'):
-                self.flashcookies_files.append((dirname, entry))
+                self.flashcookies_files.append((dirname, entry, obj['randomized']))
 
-    def get_base_path(self):
+    def add_path_if_exists(self, base_paths, base_path):
+        if os.path.exists(base_path):
+            base_paths.add(base_path)
+
+    def get_base_paths(self):
+        base_paths = set()
         self.detect_platform()
         if self.is_mac:
-            base_path = os.path.join(self.framework.get_user_home_dir(), 'Library/Preferences/Macromedia/Flash Player/#SharedObjects/')
+            self.add_path_if_exists(base_paths, os.path.join(self.framework.get_user_home_dir(), 'Library/Preferences/Macromedia/Flash Player/#SharedObjects/'))
+            self.add_path_if_exists(base_paths, os.path.join(self.framework.get_user_home_dir(), 'Library/Application Support/Google/Chrome/Default/Pepper Data/Shockwave Flash/WritableRoot/#SharedObjects/'))
         elif self.is_windows:
-            base_path = os.path.join(self.framework.get_user_home_dir(), 'Application Data\\Macromedia\\Flash Player\\#SharedObjects\\')
+            self.add_path_if_exists(base_paths, os.path.join(self.framework.get_user_home_dir(), 'Application Data\\Macromedia\\Flash Player\\#SharedObjects\\'))
+            self.add_path_if_exists(base_paths, os.path.join(self.framework.get_user_home_dir(), 'AppData\\Local\\Macromedia\\Flash Player\\#SharedObjects\\'))
+            self.add_path_if_exists(base_paths, os.path.join(self.framework.get_user_home_dir(), 'AppData\\Remote\\Macromedia\\Flash Player\\#SharedObjects\\'))
+            self.add_path_if_exists(base_paths, os.path.join(self.framework.get_user_home_dir(), 'AppData\\Local\\Google\\Chrome\\User Data\\Default\\Pepper Data\\Shockwave Flash\\WritableRoot\\#SharedObjects\\'))
+            self.add_path_if_exists(base_paths, os.path.join(self.framework.get_user_home_dir(), 'AppData\\Remote\\Google\\Chrome\\User Data\\Default\\Pepper Data\\Shockwave Flash\\WritableRoot\\#SharedObjects\\'))
         else:
             # assume linux or similar
-            base_path = os.path.join(self.framework.get_user_home_dir(), '.macromedia/Flash_Player/#SharedObjects/')
+            self.add_path_if_exists(base_paths, os.path.join(self.framework.get_user_home_dir(), '.macromedia/Flash_Player/#SharedObjects/'))
+            self.add_path_if_exists(base_paths, os.path.join(self.framework.get_user_home_dir(), '.config/google-chrome/Default/Pepper Data/Shockwave Flash/WritableRoot/#SharedObjects/'))
 
-        return base_path
+        return list(base_paths)
 
     def read_flashcookies(self):
-        base_path = self.get_base_path()
         self.flashcookies_files = []
+        for base_path in self.get_base_paths():
+            user_dir = {}
+            os.path.walk(base_path, self.visit_flashcookies_files, user_dir)
+
         self.flashcookies = {}
-        user_dir = {}
-        os.path.walk(base_path, self.visit_flashcookies_files, user_dir)
         for item in self.flashcookies_files:
             try:
-                dirname, entry = item
+                dirname, entry, randomized = item
                 filename = os.path.join(dirname, entry)
-                n = dirname.find(user_dir['randomized'])
+                print(filename)
+                n = dirname.find(randomized)
                 if n > -1:
-                    domain = dirname[n+len(user_dir['randomized'])+1:]
+                    domain = dirname[n+len(randomized)+1:]
                     n = domain.find(os.path.sep)
                     if n > -1:
                         domain = domain[:n]
